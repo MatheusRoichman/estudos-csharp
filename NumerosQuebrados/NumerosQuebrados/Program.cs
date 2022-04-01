873e7af55538dd8f0d318be6d8abd234c57b4821
Matheus Roichman <roichmankill@gmail.com>
2022-03-31 23:59:53 -0300

    Lidando com números quebrados

---
+++ NumerosQuebrados/NumerosQuebrados/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace NumerosQuebrados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Podemos limitar as casas decimais de números quebrados. Veja:
+            double num1 = 34.4663255;
+            Console.WriteLine("Número: {0}", num1);
+            
+            // o F2 limita as casas decimais para 2. Se fosse F3, por exemplo, seriam 3 casas decimais.
+            Console.WriteLine("Número com 2 casas decimais: " + num1.ToString("F2"));
+
+            Console.WriteLine($"Número com 4 casas decimais: {num1:F4}");
+        }
+    }
+}
