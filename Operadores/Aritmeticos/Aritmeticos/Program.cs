5411fdfefe55eeecac1a8229ff327fddc51d10fc
Matheus Roichman <roichmankill@gmail.com>
2022-04-01 00:02:04 -0300

    Tipos de operadores pt. 1

---
+++ Operadores/Aritmeticos/Aritmeticos/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Aritmeticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Operadores aritméticos são usados para fazer operações matemáticas.
+            
+            // Soma: +
+            int soma = 3 + 7;
+            
+            // Subtração: -
+            int subtracao = 10 - 5;
+            
+            // Multiplicação: *
+            int multiplicacao = 4 * 8;
+            
+            // Divisão: / (retorna o resultado da divisão)
+            int divisao = 20 / 4;
+            
+            // Módulo: % (retorna o resto da divisão)
+            int modulo = 10 % 6;
+            Console.WriteLine($"Resto de 10 / 6: {modulo}");
+            
+            /*
+             * ORDEM DE PRECEDÊNCIA
+             * 1. Parênteses ()
+             * 2. Módulo, divisão e multiplicação (%, /, *)
+             * 3. Soma e subtração
+             */
+
+            int op1 = 3 + 4 * 2; // 11
+            int op2 = (3 + 4) * 2; // 12
+
+            Console.WriteLine($"Sem parênteses: {op1}");
+            Console.WriteLine($"Com parênteses: {op2}");
+
+            int n1 = 10;
+            int n2 = 8;
+            double n3 = (double) n1 /n2;
+
+            Console.WriteLine($"{n1} / {n2} = {n3}");
+        }
+    }
+}
---
+++ Operadores/Atribuicao/Atribuicao/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Atribuicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Operadores de atribuição são usados para dar valores a algo.
+
+            int num;
+            
+            // num recebe 10
+            num = 10;
+            
+            // num recebe num + 2
+            num += 2;
+            
+            // O operador += também funciona para concatenação de strings
+            string str = "ABC";
+            str += "DEF";
+
+            // num recebe num - 4
+            num -= 4;
+            
+            // num recebe num * 3
+            num *= 3;
+            
+            // num recebe num / 2
+            num /= 2;
+            
+            // num recebe num % 4
+            num %= 4;
+            
+            // O operador ++ adiciona 1 ao número, e o -- subtrai 1.
+            num++;
+            num--;
+            
+            // Há diferença entre num++ e ++num, assim como num-- e --num. Iremos ver isso usando variáveis.
+            int a = 10;
+            int b = a++;
+            
+            /*
+             * Nesse exemplo, você verá no Console que 'a' será 11, mas 'b'. será 10. Por quê?
+             * Porque o ++ na frente primeiro deu o valor antigo da variável a para a variável b,
+             * e só depois disso somou 1 no valor de a.
+             */
+            Console.WriteLine($"a: {a}");
+            Console.WriteLine($"b: {b}");
+            
+            // Agora, vamos usar ainda outras variáveis.
+            int c = 20;
+            int d = ++c;
+            
+            /*
+             * Nesse caso, as 2 variáveis terão valor 21, pois o ++ atrás primeiro aumenta o valor da variável c,
+             * para depois atribuir o valor dela à variável d.
+             */
+
+            Console.WriteLine($"c: {c}");
+            Console.WriteLine($"d: {d}");
+        }
+    }
+}
