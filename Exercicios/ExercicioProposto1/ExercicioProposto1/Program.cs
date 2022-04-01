dcb8829cc2c4994a51fa4ae393d57c6d421d0e8d
Matheus Roichman <roichmankill@gmail.com>
2022-04-01 00:03:15 -0300

    Exercícios para praticar os conteúdos aprendidos

---
+++ Exercicios/ExercicioProposto1/ExercicioProposto1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace ExercicioProposto1
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int quantidadeQuartos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o preço de um produto:");
+            double precoProduto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o seu último nome, idade e altura (mesma linha):");
+            string[] valores = Console.ReadLine().Split(' ');
+
+            string ultimoNome = valores[0];
+            int idade = int.Parse(valores[1]);
+            double altura = double.Parse(valores[2]);
+
+            Console.WriteLine(nome);
+            Console.WriteLine(quantidadeQuartos);
+            Console.WriteLine(precoProduto.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(ultimoNome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Exercicios/FormulaBhaskara/FormulaBhaskara/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace FormulaBhaskara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // const impede que o valor da variável seja mudado.
+            const double a = 1.0;
+            const double b = -3.0;
+            const double c = -4.0;
+
+            // var infere o tipo da variável, dando flexibilidade ao programa.
+            
+            // Math.Pow(b, 2) é o mesmo que b * b. Os argumentos dessa função são (base, expoente).
+            var delta = Math.Pow(b, 2) - 4.0 * a * c;
+            
+            // Math.Sqrt(delta) calcula a raiz quadrada de 'delta'. Sqrt vem de Square Root (Raiz Quadrada).
+            var x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+            var x2 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+
+            Console.WriteLine("Valores:");
+            Console.WriteLine($"a: {a}");
+            Console.WriteLine($"b: {b}");
+            Console.WriteLine($"c: {c}");
+            Console.WriteLine($"O valor de delta é {delta}");
+            Console.WriteLine($"O primeiro resultado (x1) é {x1}");
+            Console.WriteLine($"O segundo resultado (x2) é {x2}");
+        }
+    }
+}
